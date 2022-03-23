40ac0148782f980e36e3eed96a3ebcb39cdf2f0c
Aironys Garrido <aironys1@hotmail.com>
2022-03-23 00:25:06 -0300

    Loop While

---
+++ script.js
@@ -0,0 +1,23 @@
+let html = '';
+let contagem = 0;
+
+while(contagem <=10){
+    html += "Número:"+contagem+ "<br/>"
+    contagem ++;
+}
+
+for(let contador2=10; contador2 <=30; contador2++){
+    html +=  "Contador 2:"+contador2+ "<br/>"
+}
+
+document.getElementById('numeros').innerHTML = html;
+document.getElementById('numeros').innerHTML = html;
+
+
+
+let mensagem = 2;
+    while(mensagem <=10){
+        mensagem ++;
+        console.log("Parabens",+mensagem)
+        }
+
